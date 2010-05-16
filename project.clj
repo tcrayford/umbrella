72a3303add7d710b587f6c704ee8a9039e4a3754
Tom Crayford <tcrayford@googlemail.com>
2010-05-17 00:18:31 +0100

    moved all dependancies to dev

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject umbrella "0.0.1"
   :description "For keeping your code DRY (cheers Giles)."
-  :dependencies [[org.clojure/clojure
-                  "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib
-                  "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
-                     [clojure_refactoring "0.1.5"]]
-  :main umbrella.core)
+  :dev-dependencies [[org.clojure/clojure
+                      "1.2.0-master-SNAPSHOT"]
+                     [org.clojure/clojure-contrib
+                      "1.2.0-SNAPSHOT"]
+                     [swank-clojure "1.2.0"]
+                     [clojure_refactoring "0.1.5"]])
