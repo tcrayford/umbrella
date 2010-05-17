e80a5fd2f352380f10637188dd6633c1a2575fad
Tom Crayford <tcrayford@googlemail.com>
2010-05-17 21:14:02 +0100

    Added the backend for ensuring variable names don't matter with regards to duplication.
    TODO: Needs a much better name than transmogrify.

--- src/umbrella/transmogrification.clj
+++ src/umbrella/transmogrification.clj
@@ -1,14 +1,56 @@
 (ns umbrella.transmogrification
-  (:use clojure.contrib.repl-utils))
+  (:use clojure.walk))
 
 (defn analyze [node]
   (clojure.lang.Compiler/analyze clojure.lang.Compiler$C/EVAL node))
 
-(defn all-vars-bound-in-node [node]
+(defmulti get-to-methods #(class (analyze %)))
+
+(defmethod get-to-methods clojure.lang.Compiler$DefExpr [node]
+  (->> (analyze node) .init .target))
+
+(defmethod get-to-methods clojure.lang.Compiler$InvokeExpr [node]
   (->> (analyze node)
-       .fexpr
+       .fexpr))
+
+(defn all-vars-bound-in-node [node]
+  (->> (get-to-methods node)
        .methods
        first
        .locals
        keys
-       (map #(.sym %))))
+       (map #(.sym %))
+       (into #{})))
+
+(defn symbol-replacements [symbols]
+  "Takes a symbol set and outputs a map of the elems of the symbol set to transmogrified variable name strings"
+  (into
+   {}
+   (map #(vector %1 (symbol (str "transmogd-" %2))) symbols (iterate inc 0))))
+
+(defn construct-replacement-node [new-node old]
+  (with-meta
+    new-node
+    {:old old}))
+
+(defn transmogrify-sub-node [node replacements]
+  (let [variable? (into #{} (keys replacements))]
+    (if (variable? node)
+      (construct-replacement-node (replacements node) node)
+      node)))
+
+(defn transmogrify [node]
+  (let [replacements (symbol-replacements (all-vars-bound-in-node node))
+        variable? (into #{} (keys replacements))]
+    (postwalk
+     #(transmogrify-sub-node % replacements)
+     node)))
+
+(defn maybe-replace-with-old-node [node]
+  (if-let [n (:old (meta node))] n node))
+
+(defn untransmogrify [transmogd-node]
+  (postwalk
+   maybe-replace-with-old-node
+   transmogd-node))
+
--- test/umbrella/transmogrification_test.clj
+++ test/umbrella/transmogrification_test.clj
@@ -4,6 +4,20 @@
 
 (deftest all_vars_bound_in_node
   (testing "defn binds"
-   (is (= (all-vars-bound-in-node '(defn a [b] b)) '(b))))
+    (is (= (all-vars-bound-in-node '(defn a [b] (let [x 1] (+ b x)))) '#{b x a})))
   (testing "let binds"
-   (is (= (all-vars-bound-in-node '(let [a 1] a)) '(a)))))
+    (is (= (all-vars-bound-in-node '(let [a 1] a)) '#{a}))))
+
+(deftest transmogrify_test
+  (is (= (transmogrify '(defn a [c] c))
+       '(defn transmogd-0 [transmogd-1] transmogd-1))))
+
+(deftest untransmogrify_test
+  (is (=
+       (untransmogrify (transmogrify '(defn a [b] b)))
+       '(defn a [b] b))
+      "Untransmogrifying a node that is transmogrified should return the original node")
+  (is (=
+       (untransmogrify '(defn a [b] b))
+       '(defn a [b] b))
+      "Untransmogrifying a node that isn't transmogrified should just return the node"))
